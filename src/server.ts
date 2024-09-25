0cfe5400afe5be560658da8873ca8402b8504105
shaharsol <shahar@gnift.com>
2024-09-25 18:02:26 +0300

    initial

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import { Server, Socket } from "socket.io";
+import products from './products.json'
+
+const io = new Server({
+  cors: {
+    origin: "*"
+  }
+});
+
+function alwaysEmit(socket: Socket) {
+
+    const index = Math.floor(Math.random() * products.length)
+
+    socket.emit('new product', products[index])
+
+    console.log('emitted')
+    const sleep = Math.random() * 1000
+    console.log(`emitting again in ${sleep} milliseconds`)
+    setTimeout(alwaysEmit, sleep)
+}
+
+
+io.on('connection', (socket) => {
+    console.log(`a user connected`);
+
+    alwaysEmit(socket)
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+});
+
+io.listen(3033)
+console.log('io server started')
